22eba9bf18791b3002da615dbddc296e27bee4c8
sophiecwebster <sophiecwebster@gmail.com>
2020-01-30 12:50:51 -0500

    set up climate for loop

--- phenocam.R
+++ phenocam.R
@@ -1,9 +1,16 @@
-install.packages("phenocamapi")
+require(phenocamapi)
+require(rgdal)
+require(sp)
 library(tidyverse)
 library(dplyr)
 library(phenocamapi)
 library(jpeg)
 library(data.table)
+require(daymetr)
+require(fields)
+require(raster)
+
+# setting up phenocam data
 
 # import pheno metadata as table
 meta <- get_phenos()
@@ -32,6 +39,48 @@ all.sites <- as.data.frame(meta)[choose.sites,]
 
 # find sites with corresponding flux tower data
 ttower <- group_by(all.sites, flux_data = TRUE)
-hi <- group_by(ttower, Veg == "QURU")
+quru <- group_by(ttower, Veg == "QURU")
+
+view(ttower)
 
 # 36 sites are ACRU only, 16 are QURU only, 15 are both
+# save out data
+
+#sites    <- ttower[which(ttower$Veg == "QURU" | ttower$Veg == "BOTH"),]
+#site_map <- sites
+#coordinates(site_map) = ~lon+lat
+#proj4string(site_map) <- CRS("+proj=longlat +datum=WGS84")
+
+#########################
+# Extracting Climate Data
+#########################
+
+# using only sites with co-located flux tower; pulling site name, lat, lon 
+# (all.sites is used in fig 1)
+
+sites <- all.sites
+latlon <- sites[c("lat", "lon", "site")]
+
+# Pull Climate Data
+
+# loop through sites and gather data
+climate_results <- c()
+for(j in 1:nrow(latlon)) {
+  
+  print(j)
+
+  # Variables
+  years <- 1980:2017
+  Latitude <- latlon[j, "lat"]
+  Longitude <- latlon[j, "lon"]
+  
+  #Download climate data
+  climate <- download_daymet(site = "Oak Ridge National Laboratories",
+                             lat = Latitude,
+                             lon = Longitude,
+                             start = years[1],
+                             end = years[length(years)],
+                             internal = TRUE, force = T, silent = T)
+  climate.data <- climate$data
+
+}
