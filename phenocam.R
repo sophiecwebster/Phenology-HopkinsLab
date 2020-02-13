3e1864abe0268e90370ce0bf74cf5b53b766faff
sophiecwebster <sophiecwebster@gmail.com>
2020-02-13 11:27:08 -0500

    finished climate data loop

--- phenocam.R
+++ phenocam.R
@@ -73,25 +73,68 @@ for(j in 1:nrow(latlon)) {
   years <- 1980:2018
   Latitude <- latlon[j, "lat"]
   Longitude <- latlon[j, "lon"]
+  print(Latitude)
+   
+   #Download climate data
+   climate <- download_daymet(site = "Oak Ridge National Laboratories",
+                              lat = Latitude,
+                              lon = Longitude,
+                              start = years[1],
+                              end = years[length(years)],
+                              internal = TRUE, force = T, silent = T)
   
-  #Download climate data
-  climate <- download_daymet(site = "Oak Ridge National Laboratories",
-                             lat = Latitude,
-                             lon = Longitude,
-                             start = years[1],
-                             end = years[length(years)],
-                             internal = TRUE, force = T, silent = T)
-  climate.data <- climate$data
-
-  #Pull out month and day info
+   climate.data <- climate$data
+   
+   #Pull out month and day info
+   
+   date.list <- c()
+   for(year in 1:length(years)){
+     DOY <- climate.data$yday[climate.data$year == years[year]] #<- what is "years" doing here?
+     date <- as.Date(DOY, origin = paste0(years[year], "-01-01"))
+     date.list <- c(date.list, as.character(date))
+   }
+   date.list <- as.Date(date.list)
+   month <- as.numeric(format(date.list, "%m"))
+   climate.data$month <- month
+   
+   ### Climate Variables
+   
+   # Mean Temp (each day)
+   climate.data$tmean <- rowMeans(data.frame(climate.data$tmin..deg.c., climate.data$tmax..deg.c.))
+   
+   # Annual Precip
+   annual_precip <- aggregate(prcp..mm.day. ~ year, data = climate.data, sum)
+   annual_precip <- mean(annual_precip$prcp..mm.day.)
+   
+   # Yearly Averages (MAT and MAP) -- MAP or AP?
+   gb_yr <- group_by(climate.data, year)
+   climate.year <- summarize(gb_yr, MAT = mean(tmean, na.rm = T), MAP = sum(prcp..mm.day.), MSRAD = sum(srad..W.m.2.), MIN = min(tmin..deg.c.), MAX = max(tmax..deg.c.))
+   
+   # Monthly Averages
+   gb_m <- group_by(climate.data, year, month)
+   climate.month <- summarize(gb_m, MMAT = mean(tmean, na.rm = T), MMAP = sum(prcp..mm.day.), MMSRAD = sum(srad..W.m.2.), MMIX = min(tmin..deg.c.), MMAX = max(tmax..deg.c.))
   
-  date.list <- c()
-  for(year in 1:length(years)){
-    DOY <- climate.data$yday[climate.data$year == years[year]]
-    date <- as.Date(DOY, origin = paste0(years[year], "-01-01"))
-    date.list <- c(date.list, as.character(date))
-  }
-  date.list <- as.Date(date.list)
-  month <- as.numeric(format(date.list, "%m"))
-  climate.data$month <- month
+   # Calc Vars
+   MAT <- mean(climate.year$MAT)
+   MAP <- mean(climate.year$MAP)
+   MSRAD <- mean(climate.year$MSRAD)
+   MIN <- mean(climate.year$MIN)
+   MAX <- mean(climate.year$MAX)
+   Daylength_7_1 <- climate.data$dayl..s.[climate.data$year == 2018 & climate.data$yday == 182] / 60 / 60
+   Daylength_12_21 <- climate.data$dayl..s.[climate.data$year == 2018 & climate.data$yday == 355] / 60 / 60
+     
+   # Save Results 
+   climate.vars <- data.frame(latlon[j, "site"], Latitude, Longitude, MAT, MAP, MSRAD, MIN, MAX, Daylength_12_21, Daylength_7_1)
+   
+   # Add Rows
+   climate_results <- rbind(climate_results, climate.vars) #are we binding climate_results or climate.data here?
 }
+
+
+
+
+
+
+
+
+
