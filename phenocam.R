2cde9fab62d3455f309373ec697b91e169a149f0
sophiecwebster <sophiecwebster@gmail.com>
2020-02-27 00:31:29 -0500

    working on phenocam script

--- phenocam.R
+++ phenocam.R
@@ -1,5 +1,86 @@
 ############################################
-########## USER INPUT ######################
+######## PHENOCAM DATA RETRIEVAL ###########
 ############################################
 
 # Load packages
+library(tidyverse)
+library(phenocamapi)
+library(rgdal)
+library(sp)
+library(dplyr)
+library(jpeg)
+library(data.table)
+library(fields)
+library(raster)
+library(data.table)
+
+meta <- get_phenos()
+
+# make lowercase
+meta$dominant_species <- tolower(meta$dominant_species)
+
+# find rows of sites where predominant species is acer rubrum
+acer.sites <- grep("acer rubrum", meta$dominant_species)
+
+# find rows of sites where predominant species is quercus rubra
+queru.sites <- grep("quercus rubra", meta$dominant_species)
+queru.sites <- c(queru.sites, grep("quercus sp", meta$dominant_species))
+both.sites <- intersect(acer.sites, queru.sites)
+
+# add column for vegetation type (either acer, quercus, or both)
+meta$Veg <- "ACRU"
+meta$Veg[queru.sites] <- "QURU"
+meta$Veg[both.sites] <- "BOTH"
+
+# choose sites of interest, make into data table
+choose.sites <- c(acer.sites,queru.sites)
+choose.sites <- as.numeric(sort(choose.sites))
+choose.sites <- unique(choose.sites)
+all.sites <- as.data.frame(meta)[choose.sites,]
+
+# obtaining the list of all the available ROI's on the PhenoCam server
+rois <- get_rois()
+
+sites_rois <- rois %>% right_join(all.sites, by = c("site" = "site", "lat" = "lat", "lon" = "lon"))
+
+ts_sites <- rois %>% right_join(all.sites, by = c("site" = "site", "lat" = "lat", "lon" = "lon")) %>%
+    filter(!is.na(roitype))
+
+
+#for loop it! do we want this down to 1 day or 3 day res?
+# going thru everything but not saving it all -- only last guy
+# want site name to be level name of this mega factor? i think?
+
+time_series <- c()
+for(i in 1:nrow(sites_rois)) {
+  site <- ts_sites$site
+  ts <- get_pheno_ts(site = ts_sites$site[i], vegType = ts_sites$roitype[i], roiID = ts_sites$sequence_number[i], type = '1day')
+  time_series <- c(ts)
+}
+
+
+# now just checking out alligatorriver site to see how it looks/graphin'
+gator <- get_pheno_ts(site = sites_rois$site[1], vegType = sites_rois$roitype[1], roiID = sites_rois$sequence_number[1], type = '1day') %>% 
+    filter(!is.na(gcc_90))
+
+gator$date <- as.Date(gator$date)
+
+gator %>% ggplot(aes(date, gcc_90)) + geom_point(alpha = 0.2, color = "green") + labs(x = "", y = "GCC 90") +
+  scale_x_date(date_breaks = "1 year")
+
+springyd <- gator$date[1604:1674]
+gspringy <- gator$gcc_90[1604:1674]
+springyd <- as.Date(springyd)
+hallo <- cbind(springyd, gspringy)
+hallo <- as.data.frame(hallo)
+
+hallo %>% ggplot(aes(springyd, gspringy)) + geom_point(alpha = 0.5, color = "green") + labs(x = "", y = "GCC 90") #+
+  #scale_x_date()
+
+
+# eventually
+# need to convert from factor to date
+var_[,date:=as.Date(date)]
+
+
+
